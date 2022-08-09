34453c911a254256108c72919f5f5188e005bbc5
Xvr35a <103354662+Xvr35a@users.noreply.github.com>
2022-08-09 16:12:05 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -63,7 +63,7 @@ async def filter(client, message):
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"pr0fess0r_99#{file_id}")]
                     )
         else:
-            await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')
+            await client.send_sticker(chat_id=message.from_user.id, sticker='CAACAgUAAxkBAAERXzZi8jm1Pqdihl0AAfITSK9Nvl')
             return
 
         if not btn:
